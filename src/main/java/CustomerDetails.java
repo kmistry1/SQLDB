f1d473d6cc01dd13734e20b5a0c273ac7a56cfd3
sunny <none>
2020-10-10 11:23:45 -0400

    changes to file

--- src/main/java/CustomerDetails.java
+++ src/main/java/CustomerDetails.java
@@ -3,19 +3,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CustomerDetails {
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/ITCS6112";
-    static final String USER = "root";
-    static final String PASS = "password";
-    static final String sqlQuery = "SELECT * FROM ITCS6112.CustomerDetails";
-
 
     public static void main(String[] args) {
         ResultSet rs = null;
         try {
-            Class.forName(JDBC_DRIVER);
-            rs = DriverManager.getConnection(DB_URL, USER, PASS)
-                    .createStatement().executeQuery(sqlQuery);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            rs = DriverManager.getConnection("jdbc:mysql://localhost:3306/ITCS6112", "root", "password")
+                    .createStatement().executeQuery("SELECT * FROM ITCS6112.CustomerDetails");
             while (rs.next()) {
                 printoutData(rs);
             }
@@ -37,16 +31,7 @@ public class CustomerDetails {
     }
 
     private static String toString(ResultSet rs) throws SQLException {
-        return "************ Customer Detail ["
-                + getTableData(rs, "PersonID")
-                + "] ************" + "\n" + "Full Name: "
-                + getTableData(rs, "FirstName")
-                + " " + getTableData(rs, "LastName")
-                + "\n" + "Address: " + getTableData(rs, "Address")
-                + "\n" + getTableData(rs, "City") + ", "
-                + getTableData(rs, "State") + " "
-                + getTableData(rs, "ZipCode") + " "
-                + getTableData(rs, "Country") + "\n";
+        return "************ Customer Detail [" + getTableData(rs, "PersonID") + "] ************" + "\n" + "Full Name: " + getTableData(rs, "FirstName") + " " + getTableData(rs, "LastName") + "\n" + "Address: " + getTableData(rs, "Address") + "\n" + getTableData(rs, "City") + ", " + getTableData(rs, "State") + " " + getTableData(rs, "ZipCode") + " " + getTableData(rs, "Country") + "\n";
     }
 
     private static String getTableData(ResultSet rs, String personID) throws SQLException {
